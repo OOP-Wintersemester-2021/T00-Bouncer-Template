071d3767a70def590e910ec64aa5a11dcd9df6fe
Alexander Bazo <alexanderbazo@googlemail.com>
2021-10-30 21:32:55 +0200

    Add template code

---
+++ src/$APPLICATION_NAME.java
@@ -0,0 +1,15 @@
+import de.ur.mi.bouncer.apps.BouncerApp;
+import de.ur.mi.bouncer.apps.BouncerLauncher;
+
+
+public class BouncerAlsBergsteiger extends BouncerApp {
+
+    @Override
+    public void bounce() {
+        loadMap("$MAP");
+    }
+
+    public static void main(String[] args) {
+        BouncerLauncher.launch("$APPLICATION_NAME");
+    }
+}
